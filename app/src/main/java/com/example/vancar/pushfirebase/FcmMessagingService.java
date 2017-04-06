780d44fed2afdf808634fb726196e38679b624f1
Carlos Buruel <buruel.carlos@gmail.com>
2017-04-06 17:39:20 -0500

    comentarios y custom de push

--- app/src/main/java/com/example/vancar/pushfirebase/FcmMessagingService.java
+++ app/src/main/java/com/example/vancar/pushfirebase/FcmMessagingService.java
@@ -1,12 +1,16 @@
 package com.example.vancar.pushfirebase;
 
-import android.widget.Toast;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.app.NotificationCompat;
 
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 
 /**
- *
+ * @author Carlos Buruel
  */
 
 public class FcmMessagingService extends FirebaseMessagingService
@@ -17,6 +21,16 @@ public class FcmMessagingService extends FirebaseMessagingService
 		String title = remoteMessage.getNotification().getTitle();
 		String message = remoteMessage.getNotification().getBody();
 
-		System.out.println(title + ", "+ message);
+		Intent intent = new Intent(this, MainActivity.class);
+		intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+		PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_ONE_SHOT);
+		NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this);
+		notificationBuilder.setContentTitle(title);
+		notificationBuilder.setContentText(message);
+		notificationBuilder.setSmallIcon(R.drawable.ic_eagle);
+		notificationBuilder.setAutoCancel(true);
+		notificationBuilder.setContentIntent(pendingIntent);
+		NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+		notificationManager.notify(0, notificationBuilder.build());
 	}
 }
